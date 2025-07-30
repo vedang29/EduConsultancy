fe8b436940c3423d5301ce7e45910813024ef00f
vedang29 <vedang294@gmail.com>
2025-07-30 11:17:37 +0530

    Configured for deployement

--- frontend/src/services/helper.js
+++ frontend/src/services/helper.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 // export const BASE_URL='http://localhost:8080';
-export const BASE_URL='http://localhost:5171';
+// export const BASE_URL='http://localhost:5171';
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL;
 
 export const myAxios=axios.create({
     baseURL:BASE_URL
